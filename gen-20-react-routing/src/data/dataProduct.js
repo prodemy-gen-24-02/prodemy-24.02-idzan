bdcb023befea0f2ca1cd5cca18837c7ded038c0e
Idzan Aufar <aufar.idzan@gmail.com>
2024-05-27 21:38:53 +0700

    first commit

---
+++ gen-20-react-routing/src/data/dataProduct.js
@@ -0,0 +1,123 @@
+import produk1 from "../assets/produk/produk1.png";
+import produk1_1 from "../assets/produk/produk1.1.jpg";
+import produk1_2 from "../assets/produk/produk1.2.jpeg";
+import produk1_3 from "../assets/produk/produk1.3.png";
+import produk2 from "../assets/produk/produk2.jpg";
+import produk2_1 from "../assets/produk/produk2.1.jpg";
+import produk2_2 from "../assets/produk/produk2.2.jpg";
+import produk2_3 from "../assets/produk/produk2.3.jpg";
+import produk3 from "../assets/produk/produk3.png";
+import produk3_1 from "../assets/produk/produk3.1.jpg";
+import produk3_2 from "../assets/produk/produk3.2.png";
+import produk3_3 from "../assets/produk/produk3.3.jpg";
+import produk4 from "../assets/produk/produk4.png";
+import produk4_1 from "../assets/produk/produk4.1.jpg";
+import produk4_2 from "../assets/produk/produk4.2.jpg";
+import produk4_3 from "../assets/produk/produk4.3.jpg";
+import produk5 from "../assets/produk/produk5.png";
+import produk5_1 from "../assets/produk/produk5.1.jpg";
+import produk5_2 from "../assets/produk/produk5.2.jpg";
+import produk5_3 from "../assets/produk/produk5.3.jpg";
+import produk6 from "../assets/produk/produk6.png";
+import produk6_1 from "../assets/produk/produk6.1.jpg";
+import produk6_2 from "../assets/produk/produk6.2.jpg";
+import produk6_3 from "../assets/produk/produk6.3.jpg";
+import produk7 from "../assets/produk/produk7.jpg";
+import produk7_1 from "../assets/produk/produk7.1.jpg";
+import produk7_2 from "../assets/produk/produk7.2.jpg";
+import produk7_3 from "../assets/produk/produk7.3.jpg";
+import produk8 from "../assets/produk/produk8.jpg";
+import produk8_1 from "../assets/produk/produk8.1.jpg";
+import produk8_2 from "../assets/produk/produk8.2.jpg";
+import produk8_3 from "../assets/produk/produk8.3.jpg";
+
+export const DataProduct = [
+  {
+    id: 1,
+    name: "Vivobook 14X (M1403)",
+    images: [produk1, produk1_1, produk1_2, produk1_3],
+    alt: "Laptop Vivobook 14x",
+    price: "Rp. 7.999.000",
+    rating: 4,
+    description:
+      "Vivobook 14X adalah laptop yang ideal untuk produktivitas dan hiburan. Dengan performa tinggi dan desain yang stylish, Vivobook 14X siap mendukung aktivitas sehari-hari Anda.",
+    category: "Komputer",
+  },
+  {
+    id: 2,
+    name: "PlayStation 5 (PS 5)",
+    images: [produk2, produk2_1, produk2_2, produk2_3],
+    alt: "PS 5",
+    price: "Rp. 8.999.000",
+    rating: 5,
+    description:
+      "PS5 adalah konsol game generasi terbaru yang menawarkan pengalaman gaming terdepan. Dibuat oleh Sony, PS5 memiliki kekuatan grafis dan performa yang luar biasa, menjadikannya pilihan utama para gamer.",
+    category: "Video games",
+  },
+  {
+    id: 3,
+    name: "Infinix Note 40 Pro 5G 8+256GB",
+    images: [produk3, produk3_1, produk3_2, produk3_3],
+    alt: "Hp Infinix Note 40",
+    price: "Rp. 3.700.000",
+    rating: 4,
+    description:
+      "Infinix Note 40 Pro adalah smartphone canggih dengan kapasitas penyimpanan 8+256GB. Dengan teknologi 5G, Anda dapat menikmati kecepatan internet yang luar biasa.",
+    category: "Smartphone",
+  },
+  {
+    id: 4,
+    name: "Nintendo Switch",
+    images: [produk4, produk4_1, produk4_2, produk4_3],
+    alt: "Nintendo Switch",
+    price: "Rp. 5.000.000",
+    rating: 4,
+    description:
+      "Nintendo Switch adalah konsol game revolusioner yang dapat digunakan baik sebagai perangkat genggam maupun terhubung ke TV. Nikmati pengalaman bermain yang seru dan fleksibel.",
+    category: "Video games",
+  },
+  {
+    id: 5,
+    name: "Adjustable Dumbbell Set",
+    images: [produk5, produk5_1, produk5_2, produk5_3],
+    alt: "Dumbell fleksibel",
+    price: "Rp. 7.800.000",
+    rating: 4,
+    description:
+      "Set Dumbbell Adjustable memungkinkan Anda untuk menyesuaikan berat dengan mudah, menjadikannya pilihan yang sempurna untuk latihan kekuatan di rumah.",
+    category: "Olahraga",
+  },
+  {
+    id: 6,
+    name: "Smart Watch v2",
+    images: [produk6, produk6_1, produk6_2, produk6_3],
+    alt: "Smart watch v2",
+    price: "Rp. 900.000",
+    rating: 3,
+    description:
+      "Smart Watch v2 adalah jam tangan pintar yang dilengkapi dengan berbagai fitur kesehatan dan kebugaran. Tetap terhubung dan pantau kesehatan Anda dengan mudah.",
+    category: "Smartwatch",
+  },
+  {
+    id: 7,
+    name: "Xbox Series X",
+    images: [produk7, produk7_1, produk7_2, produk7_3],
+    alt: "Xbox Series X",
+    price: "Rp. 6.700.000",
+    rating: 5,
+    description:
+      "Xbox Series X adalah konsol game generasi terbaru dari Microsoft yang menawarkan pengalaman gaming terbaik dengan grafis memukau dan performa tinggi.",
+    category: "Video games",
+  },
+  {
+    id: 8,
+    name: "Playstation 4",
+    images: [produk8, produk8_1, produk8_2, produk8_3],
+    alt: "Playstation 4",
+    price: "Rp. 4.000.000",
+    rating: 3,
+    description:
+      "Playstation 4 adalah konsol game populer dari Sony yang menawarkan berbagai game eksklusif dan pengalaman gaming yang imersif.",
+    category: "Video games",
+  },
+];
---
+++ gen-20-react-routing/src/pages/ProductDetail.jsx
@@ -0,0 +1,63 @@
+import { useParams } from "react-router-dom";
+import Layout from "../Layout/Layout";
+
+import ProductSection from "../components/ProductSection";
+import ProdukSerupa from "../components/ProdukSerupa";
+import { DataProduct } from "../data/dataProduct";
+
+function ProductDetail() {
+  const { id } = useParams();
+  const product = DataProduct.find((prod) => prod.id === parseInt(id));
+
+  // if (!product) {
+  //   return (
+  //     <Layout>
+  //       <div className="pt-20  w-full border  p-6  items-center text-center">
+  //         <h2 className="text-2xl font-bold mb-4 text-center font-roboto">
+  //           Produk Tidak Ditemukan
+  //         </h2>
+
+  //         <dotlottie-player
+  //           src="https://lottie.host/c71118ee-d86e-4d9f-8bf4-80830a6a5ca9/DHxUGxa19V.json"
+  //           background="transparent"
+  //           speed="1"
+  //           style={{ width: "300px", height: "300px", margin: "0 auto" }}
+  //           loop
+  //           autoplay
+  //         ></dotlottie-player>
+
+  //         <p className="text-gray-700 mt-4">
+  //           Maaf, produk yang Anda cari tidak ditemukan. Silakan cek kembali
+  //           atau lihat produk lainnya yang mungkin menarik bagi Anda.
+  //         </p>
+  //       </div>
+  //     </Layout>
+  //   );
+  // }
+
+  const relatedProducts = DataProduct.filter(
+    (prod) => prod.category === product.category && prod.id !== product.id
+  );
+
+  return (
+    <Layout>
+      <ProductSection product={product} />
+      {relatedProducts.length > 0 ? (
+        <ProdukSerupa produk={relatedProducts} />
+      ) : (
+        <div className="my-8 mx-auto max-w-6xl p-6 text-center">
+          <h2 className="text-2xl font-bold mb-4 text-center font-roboto">
+            Produk Serupa Tidak Tersedia
+          </h2>
+          <p className="text-gray-700">
+            Maaf, tidak ada produk serupa yang tersedia saat ini. Silakan cek
+            kembali kapan-kapan atau lihat produk lainnya yang mungkin menarik
+            bagi Anda.
+          </p>
+        </div>
+      )}
+    </Layout>
+  );
+}
+
+export default ProductDetail;
